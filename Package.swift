3c47e6dc28214ef59eebe11d55c26a0e2ef7b1e1
Aashutosh Shrestha <aashutoshrestha@outlook.com>
2024-04-15 12:41:13 +0545

    Added Minimum iOS Version

--- Package.swift
+++ Package.swift
@@ -5,20 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "UXAnalytics",
+    platforms: [
+        .iOS(.v13)
+        ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "UXAnalytics",
-            targets: ["UXAnalytics"]),
-        .library(name: "Persistence",
-                 targets: ["Persistence"])
+            targets: ["UXAnalytics"])
         
     ],
+    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "UXAnalytics"),
+        
         .testTarget(
             name: "UXAnalyticsTests",
             dependencies: ["UXAnalytics"]),
