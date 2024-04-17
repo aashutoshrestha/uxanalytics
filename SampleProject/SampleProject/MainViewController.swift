d2679e5bc2cd82b39973e65e5613d838d0749885
Aashutosh Shrestha <aashutoshrestha@outlook.com>
2024-04-17 17:38:45 +0545

    Changes to sample project

--- SampleProject/SampleProject/MainViewController.swift
+++ SampleProject/SampleProject/MainViewController.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+import UXAnalytics
+enum UXAnalyticsError: Error{
+    case recordEventError
+}
+class MainViewController: UXAnalyticsViewController{
+   
+    override func viewDidLoad() {
+      
+    }
+    
+    func initializeAnalytics(){
+        initAnalytics()
+    }
+    
+    func startAnalyticsSession(){
+        startSession()
+    }
+    
+    func stopAnalyticsSession(){
+        stopSession()
+    }
+    
+    func recordAnalyticsEvents(name eventName: String, properties eventProperties: [String:String]) throws {
+        do{
+            try recordEvent(name: eventName, property: eventProperties)
+        }catch{
+            throw UXAnalyticsError.recordEventError
+        }
+    }
+    
+    func getSessions() throws -> [Sessions]{
+        do{
+            return try getSessionData()
+        }catch let ex{
+            print(ex.localizedDescription)
+        }
+        return [Sessions]()
+    }
+    
+    func getEvents(session: String) throws -> [Events]{
+        do{
+            return try getEventsData(session: session)
+        }catch let ex{
+            print(ex.localizedDescription)
+        }
+        return [Events]()
+    }
+}
--- SampleProject/SampleProject/ViewController.swift
+++ SampleProject/SampleProject/ViewController.swift
@@ -4,17 +4,58 @@
 //
 //  Created by Aashutosh Shrestha on 4/17/24.
 //
-import UXAnalytics
 import UIKit
+class ViewController: MainViewController {
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var startSessionBtn: UIButton!
+    @IBOutlet weak var recordEventBtn: UIButton!
+    
+    var sessionStarted: Bool = false
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        do{
+            print(try getSessions())
+        }catch{
+            
+        }
     
     }
-
+    @IBAction func recordEvent(_ sender: Any) {
+        let alert = UIAlertController(title: "Select Event", message: "Add Event", preferredStyle: .alert)
+        for i in ["Event 1", "Event 2", "Event 3", "Event 4"] {
+//            alert.addAction(UIAlertAction(title: i, style: .Default, handler: doSomething()))
+            alert.addAction(UIAlertAction(title: i, style: .default,handler: { action in
+                do{
+                    var eventProperties = [
+                        "date": Date().formatted(),
+                    ]
+                    try self.recordAnalyticsEvents(name: i, properties: eventProperties)
+//                    print(getEvents(session: <#T##String#>))
+                }catch{
+                    
+                }
+            }))
+        }
+                            self.present(alert, animated: true, completion: nil)
+    }
+    @IBAction func startSession(_ sender: Any) {
+        if self.sessionStarted{
+            self.sessionStarted = false
+            self.startSessionBtn.setTitle("Start Session", for: .normal)
+            stopAnalyticsSession()
+        }else{
+            self.sessionStarted = true
+            self.startSessionBtn.setTitle("Stop Session", for: .normal)
+            startAnalyticsSession()
+        }
+        
+        do{
+            print(try getSessions())
+        }catch{
+            
+        }
+    }
+    
 
 }
 
