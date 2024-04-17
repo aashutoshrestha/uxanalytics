9c5e62733fef088ad8f38df37b5b128d73107310
Aashutosh Shrestha <aashutoshrestha@outlook.com>
2024-04-17 10:11:17 +0545

    Added sample Project

---
+++ SampleProject/SampleProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleProject
+//
+//  Created by Aashutosh Shrestha on 4/17/24.
+//
+import UXAnalytics
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+    }
+
+
+}
+
