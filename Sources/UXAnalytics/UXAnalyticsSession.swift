1ce3a4092238f4d5e93746cca76c4e0001d03987
Aashutosh Shrestha <aashutoshrestha@outlook.com>
2024-04-18 11:29:34 +0545

    Updated Session add

--- Sources/UXAnalytics/UXAnalyticsSession.swift
+++ Sources/UXAnalytics/UXAnalyticsSession.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CoreData
 public enum SessionError: Error{
     case sessionStartError
     case sessionEndError
@@ -26,6 +27,11 @@ class UXAnalyticsSession{
     }
     func startSession(){
         startTime = Date()
+        do{
+            try recordSession()
+        }catch let ex{
+            print("Record Start Session", ex.localizedDescription)
+        }
     }
     func recordEvent(name eventName: String, property eventProperties: [String:String])throws{
         let event = UXAnalyticsEvent(eventName: eventName, eventProperties: eventProperties)
@@ -49,6 +55,25 @@ class UXAnalyticsSession{
     
     func recordSession() throws{
         let context = CoreDataManager.shared.context
+        if fetchSessions() != nil && (fetchSessions()?.count ?? 0) > 1 {
+            let fetchRequest: NSFetchRequest<Sessions> = Sessions.fetchRequest()
+            fetchRequest.predicate = NSPredicate(format: "id == %@", argumentArray: [self.id]) // Adjust the predicate as per your requirements
+
+            do {
+                let results = try context.fetch(fetchRequest)
+                if let session = results.first {
+                    session.startTime = startTime
+                    session.endTime = endTime
+                    try context.save()
+                    print("Session Updated")
+                    print(fetchSessions() as Any)
+                    return
+                }
+            } catch let error as NSError {
+                print("Could not fetch or update. \(error), \(error.userInfo)")
+                return
+            }
+        }
         let session = Sessions(context: context)
         session.id = id
         session.startTime = startTime
@@ -58,9 +83,23 @@ class UXAnalyticsSession{
         do{
             try context.save()
             print("Session Saved")
+            print(fetchSessions() as Any)
         }catch let ex{
             print("Error saving sessions", ex.localizedDescription)
             throw SessionError.recordSessionError
+        }
+    }
+    
+    func fetchSessions() -> [Sessions]?{
+        let context = CoreDataManager.shared.context
+        let fetchRequest = NSFetchRequest<Sessions>(entityName: "Sessions")
+        
+        do {
+            let sessions = try context.fetch(fetchRequest)
+            return sessions
+        }catch let ex{
+            print("Error getAnalyticalsDataSessions", ex.localizedDescription)
+            return nil
         }
     }
 }
