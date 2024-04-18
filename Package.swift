0ebb42a23b99911cfc75047f201884ef96373ebe
Aashutosh Shrestha <aashutoshrestha@outlook.com>
2024-04-18 14:01:58 +0545

    Changes to coredata

--- Package.swift
+++ Package.swift
@@ -20,7 +20,11 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "UXAnalytics"),
+            name: "UXAnalytics",
+            resources: [
+                    .process("Sources/UXAnalytics/UXAnalytics.xcdatamodeld")
+                ]
+        ),
         
         .testTarget(
             name: "UXAnalyticsTests",
--- Sources/UXAnalytics/CoreDataManager.swift
+++ Sources/UXAnalytics/CoreDataManager.swift
@@ -13,6 +13,30 @@ class CoreDataManager {
     private init() {}
     
     lazy var persistentContainer: NSPersistentContainer = {
+        guard let modelURL = Bundle.module.url(forResource: "UXAnalytics", withExtension: "momd", subdirectory: "Sources/UXAnalytics") else {
+            fatalError("Failed to locate model file in bundle.")
+        }
+
+        guard let managedObjectModel = NSManagedObjectModel(contentsOf: modelURL) else {
+            fatalError("Failed to initialize managed object model from URL: \(modelURL)")
+        }
+
+        let persistentContainer = NSPersistentContainer(name: "UXAnalytics", managedObjectModel: managedObjectModel)
+
+        // Configure the persistent store coordinator
+        let description = NSPersistentStoreDescription()
+        description.url = try! FileManager.default.url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: true).appendingPathComponent("UXAnalytics.sqlite")
+        persistentContainer.persistentStoreDescriptions = [description]
+
+        // Load persistent stores
+        persistentContainer.loadPersistentStores { (storeDescription, error) in
+            if let error = error {
+                fatalError("Failed to load persistent stores: \(error)")
+            }
+        }
+        
+        return persistentContainer
+        /*
         let container = NSPersistentContainer(name: "UXAnalytics")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
@@ -20,6 +44,7 @@ class CoreDataManager {
             }
         })
         return container
+        */
     }()
     
     var context: NSManagedObjectContext {
